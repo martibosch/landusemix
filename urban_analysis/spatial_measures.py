5c0847ae22984043557954ce9c6ad07eb6bdc1f5
Marti Bosch <marti.bosch.1992@gmail.com>
2016-06-12 22:52:10 +0200

    corrected measures, added some notebooks

--- urban_analysis/spatial_measures.py
+++ urban_analysis/spatial_measures.py
@@ -23,21 +23,29 @@ import numpy as np
 #     return np.hstack(xx), np.hstack(yy), np.hstack(f), f.mean()
 
 
+def get_midpoint_grid(xx, yy):
+    x0 = xx[0]
+    y0 = yy.T[0]
+    return np.array([[s + (t - s) / 2.0 for s, t in zip(x0, x0[1:])] for i in range(len(xx) - 1)]), np.array([[s + (t - s) / 2.0 for s, t in zip(y0, y0[1:])] for i in range(len(yy.T) - 1)]).T
+
+
 def grid_cell_kde_average(f):
     """ Determines for each cell the trapezoid-based average out of the kde values of the cell's vertices
 
     :param numpy.ndarray f: 2-dimensional
-    :returns: flat array
+    :returns: 2-dimensional array
     :rtype: numpy.ndarray 
 
     """
     ff = []
     for i in range(len(f) - 1):
+        fi = []
         f_row1 = f[i]
         f_row2 = f[i + 1]
         for j in range(len(f[0]) - 1):
-            ff.append((f_row1[j] + f_row1[j + 1] +
+            fi.append((f_row1[j] + f_row1[j + 1] +
                        f_row2[j] + f_row2[j + 1]) / 4)
+        ff.append(fi)
     return np.array(ff)
 
 
@@ -47,87 +55,113 @@ def grid_cell_pois_count(pois, xx, yy):
     :param pandas.DataFrame pois: 
     :param numpy.ndarray xx: 2-dimensional array first componenent of a numpy.meshgrid() call
     :param numpy.ndarray yy: 2-dimensional array second componenent of a numpy.meshgrid() call
-    :returns: flat array
+    :returns: 2-dimensional array
     :rtype: numpy.ndarray
 
     """
     ff = []
     n_rows, n_cols = xx.shape
     for i in range(n_rows - 1):
+        fi = []
         xi = xx[i]
         yil = yy[i]
         yih = yy[i + 1]
         for j in range(n_cols - 1):
-            ff.append(len(pois[(pois['lon'] > xi[j]) & (pois['lon'] <= xi[j + 1]) & (
+            fi.append(len(pois[(pois['lon'] > xi[j]) & (pois['lon'] <= xi[j + 1]) & (
                 pois['lat'] > yil[j]) & (pois['lat'] <= yih[j])]))
+        ff.append(fi)
     return np.array(ff)
 
-
 # INDICATOR CALCULATION
+
 
 def moran_index(f, xx, yy):
     """ 
 
-    :param numpy.ndarray f: flat array
-    :param numpy.ndarray xx: flat array
-    :param numpy.ndarray yy: flat array
+    :param numpy.ndarray f: 2-dimensional array
+    :param numpy.ndarray xx: 2-dimensional array
+    :param numpy.ndarray yy: 2-dimensional array
     :returns: 
     :rtype: float
 
     """
     # x_row, y_row, f_row, f_avg = _row_avg_decomposition(xx, yy, f)
-    f_avg = f.mean()
+    xxm, yym = get_midpoint_grid(xx, yy)
+    _xxm = xxm.flatten()
+    _yym = yym.flatten()
+    _f = f.flatten()
+    f_avg = _f.mean()
     phi = [0, 0, 0]
-    for i, (x_i, y_i) in enumerate(zip(xx, yy)):
-        f_i = f[i]
-        for j, (x_j, y_j) in enumerate(zip(xx[i + 1:-1], yy[i + 1:-1]), start=i + 1):
+    for i, (x_i, y_i) in enumerate(zip(_xxm, _yym)):
+        f_i = _f[i]
+        for j, (x_j, y_j) in enumerate(zip(_xxm[i + 1:-1], _yym[i + 1:-1]), start=i + 1):
             # print('(%d,%d) against (%d,%d):' % (x_i, y_i, x_j, y_j))
+            # print(x_i, x_j)
             w = 1.0 / np.sqrt((x_i - x_j)**2 + (y_i - y_j)**2)
             # print('weight for (%d, %d) : %f' % (i, j, w))
             phi[0] += w
-            phi[1] += w * (f_i - f_avg) * (f[j] - f_avg)
+            phi[1] += w * (f_i - f_avg) * (_f[j] - f_avg)
         phi[2] += (f_i - f_avg) ** 2
         # print('completed row ' + str(i) + ' of ' + str(len(x_row)))
-    return (float(len(xx)) / phi[0]) * (phi[1] / phi[2])
+        # print(phi)
+    return (float(len(_xxm)) / phi[0]) * (phi[1] / phi[2])
 
 
 def geary_index(f, xx, yy):
     """ 
 
-    :param numpy.ndarray f: flat array
-    :param numpy.ndarray xx: flat array 
-    :param numpy.ndarray yy: flat array
+    :param numpy.ndarray f: 2-dimensional array
+    :param numpy.ndarray xx: 2-dimensional array 
+    :param numpy.ndarray yy: 2-dimensional array
     :returns: 
     :rtype: float
 
     """
     # x_row, y_row, f_row, f_avg = _row_avg_decomposition(xx, yy, f)
-    f_avg = f.mean()
+    xxm, yym = get_midpoint_grid(xx, yy)
+    _xxm = xxm.flatten()
+    _yym = yym.flatten()
+    _f = f.flatten()
+    f_avg = _f.mean()
     phi = [0, 0, 0]
-    for i, (x_i, y_i) in enumerate(zip(xx, yy)):
-        f_i = f[i]
-        for j, (x_j, y_j) in enumerate(zip(xx[i + 1:-1], yy[i + 1:-1]), start=i + 1):
+    for i, (x_i, y_i) in enumerate(zip(_xxm, _yym)):
+        f_i = _f[i]
+        for j, (x_j, y_j) in enumerate(zip(_xxm[i + 1:-1], _yym[i + 1:-1]), start=i + 1):
             # print('(%d,%d) against (%d,%d):' % (x_i, y_i, x_j, y_j))
             w = 1.0 / np.sqrt((x_i - x_j)**2 + (y_i - y_j)**2)
             # print('weight for (%d, %d) : %f' % (i, j, w))
             phi[0] += w
-            phi[1] += w * (f_i - f[j]) ** 2
+            phi[1] += w * (f_i - _f[j]) ** 2
         phi[2] += (f_i - f_avg) ** 2
         # print('completed row ' + str(i) + ' of ' + str(len(x_row)))
-    return (.5 * (len(xx) - 1) / phi[0]) * (phi[1] / phi[2])
+    return (.5 * (len(_xxm) - 1) / phi[0]) * (phi[1] / phi[2])
+
+
+def adjusted_geary(f, xx, yy):
+    """ 
+
+    :param numpy.ndarray f: 2-dimensional array
+    :param numpy.ndarray xx: 2-dimensional array 
+    :param numpy.ndarray yy: 2-dimensional array
+    :returns: 
+    :rtype: float
+
+    """
+    return -(geary_index(f, xx, yy) - 1)
 
 
 def shannon_entropy(f):
     """
 
-    :param numpy.ndarray f: flat array
+    :param numpy.ndarray f: 2-dimensional array
     :returns: 
     :rtype: float
 
     """
-    F = f.sum()
+    _f = f.flatten()
+    F = _f.sum()
     total = 0
-    for f_i in f:
+    for f_i in _f:
         total -= (f_i / F) * np.log(f_i / F)
     return total
 
@@ -135,12 +169,12 @@ def shannon_entropy(f):
 def relative_entropy(f):
     """
 
-    :param numpy.ndarray f: flat array
+    :param numpy.ndarray f: 2-dimensional array
     :returns: 
     :rtype: float
 
     """
-    return shannon_entropy(f) / np.log(len(f))
+    return shannon_entropy(f) / np.log(f.size)
 
 
 # def calculate_measure(measure_key, measure_pre, **kwargs):
--- urban_analysis/utils.py
+++ urban_analysis/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+def _create_grid_axis(_min, _max, step, dtype=np.float32):
+    return np.arange(_min, _max, step, dtype=dtype)
 
 def grid_from_bbox(bbox, grid_step):
-    return np.meshgrid(np.arange(bbox[1], bbox[3], grid_step), np.arange(bbox[0], bbox[2], grid_step))
+    return np.meshgrid(_create_grid_axis(bbox[1], bbox[3], grid_step), _create_grid_axis(bbox[0], bbox[2], grid_step))
